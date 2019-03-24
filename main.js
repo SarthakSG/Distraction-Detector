995c7c3348c9fa8bf686744bb2389d072019930b
Sarthak <sarthak.sg@gmail.com>
2019-03-24 11:13:56 +0530

    Initial Commit

--- main.js
+++ main.js
@@ -6,32 +6,21 @@ let win;
 
 app.on('ready', () => {
 
-    win = new BrowserWindow({
-        width: 1600,
-        height: 700,
-        icon:'icons/icon.png'
-    });
-
+    win = new BrowserWindow();
+    win.maximize()
     win.loadURL("http://localhost:3000/");
     win.setMenu(null);
 
-   
 
-    win.on('closed',()=>{
-        win=null;
+
+    win.on('closed', () => {
+        win = null;
         app.quit();
     })
 
-    ipcMain.on('start',()=>{
-        win.loadURL(url.format({
-            pathname: path.join(__dirname, 'views/problem.html'),
-            protocol: 'file',
-            slashes: true
-        }));
-    })
 });
 
-app.on('window-all-closed',()=>{
-    if(process.platform!=='darwin'){app.quit()}
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') { app.quit() }
 })
 
